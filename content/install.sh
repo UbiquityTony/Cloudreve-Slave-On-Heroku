a356824fece72b7a75ccacb2fb87666e28d7722f
UbiquityTony <107668802+UbiquityTony@users.noreply.github.com>
2022-07-16 21:34:31 +0800

    Update install.sh

--- content/install.sh
+++ content/install.sh
@@ -1,5 +1,29 @@
 #/bin/sh
 
+check_sys() {
+  if [[ -f /etc/redhat-release ]]; then
+    release="centos"
+  elif grep -q -E -i "debian" /etc/issue; then
+    release="debian"
+  elif grep -q -E -i "ubuntu" /etc/issue; then
+    release="ubuntu"
+  elif grep -q -E -i "centos|red hat|redhat" /etc/issue; then
+    release="centos"
+  elif grep -q -E -i "Arch|Manjaro" /etc/issue; then
+    release="archlinux"
+  elif grep -q -E -i "debian" /proc/version; then
+    release="debian"
+  elif grep -q -E -i "ubuntu" /proc/version; then
+    release="ubu"
+  elif grep -q -E -i "centos|red hat|redhat" /proc/version; then
+    release="centos"
+  else
+    echo -e "ServerStatus 暂不支持该Linux发行版"
+  fi
+    echo -e "$release"
+
+}
+
 # Install Cloudreve
 VERSION="$(curl --retry 10 --retry-max-time 60 https://api.github.com/repos/cloudreve/Cloudreve/releases/latest | jq .tag_name | sed 's/\"//g')"
 wget -qO - https://github.com/cloudreve/Cloudreve/releases/download/${VERSION}/cloudreve_${VERSION}_linux_amd64.tar.gz | tar -zxf - -C /usr/bin
