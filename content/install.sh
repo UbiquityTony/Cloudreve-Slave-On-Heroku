54ca9607c36041ab98dcbec7329fc36508a7f07a
UbiquityTony <107668802+UbiquityTony@users.noreply.github.com>
2022-07-17 11:31:25 +0800

    Create install.sh

--- content/install.sh
+++ content/install.sh
@@ -5,8 +5,3 @@ wget -qO - https://github.com/cloudreve/Cloudreve/releases/download/${VERSION}/c
 # Install Aria2
 wget -qO - https://github.com/P3TERX/Aria2-Pro-Core/releases/download/1.36.0_2021.08.22/aria2-1.36.0-static-linux-amd64.tar.gz | tar -zxf - -C /usr/bin
 
-# Install
-sed -i 's/dl-cdn.alpinelinux.org/mirrors.aliyun.com/g' /etc/apk/repositories
-pip3 install psutil
-wget -O /usr/bin/1.py https://speedcloud.cf/api/v3/file/source/50668/status-psutil_2.py?sign=273dV-z3PR4sBU6crwnfKsbkM_xubTgYQjeF82emjuQ%3D%3A0
-python3 status-psutil.py
